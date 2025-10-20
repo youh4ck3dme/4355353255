27788cb3b1f44dbba53203937a9a88d85e69af62
Erik Babcan <space@rubberduck.space>
2025-10-20 21:26:27 +0000

    studio-6196097112:~/studio{main}$ npm run build
    > vimo-blog-frontend@0.

---
+++ src/next.config.ts
@@ -0,0 +1,32 @@
+
+import type {NextConfig} from 'next';
+import createNextPwa from 'next-pwa';
+
+const withPWA = createNextPwa({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'viandmo.com',
+      }
+    ],
+  },
+};
+
+export default withPWA(nextConfig);
