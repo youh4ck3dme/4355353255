aa5a75a18f9289c567a99eb4e457a8b7a8e6a94f
Erik Babcan <space@rubberduck.space>
2025-10-20 18:52:00 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -8,23 +8,6 @@ const withPWA = createNextPwa({
   skipWaiting: true,
   // Povolí PWA iba v produkčnom prostredí
   disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'https-calls',
-        networkTimeoutSeconds: 15,
-        expiration: {
-          maxEntries: 150,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 dní
-        },
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
-  ],
 });
 
 const nextConfig: NextConfig = {
