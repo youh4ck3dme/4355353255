fdf0fc2462e81f9fd9041eac28db6b6a272e1b88
Erik Babcan <space@rubberduck.space>
2025-10-20 11:07:01 +0000

    okej pokracj v oprave

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 
 import type {NextConfig} from 'next';
+import createNextPwa from 'next-pwa';
+
+const withPWA = createNextPwa({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
@@ -26,4 +35,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
