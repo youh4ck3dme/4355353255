1f5ee1f5724f4d709de3e366ddb95a1821045be4
Erik Babcan <space@rubberduck.space>
2025-10-23 10:47:15 +0000

    pokracuj

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 
 /** @type {import('next').NextConfig} */
+import createNextPwa from 'next-pwa';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')({
+const withPWA = createNextPwa({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-})
+  disable: process.env.NODE_ENV === 'development',
+});
 
 const nextConfig = {
   reactStrictMode: true,
@@ -16,13 +16,17 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'viandmo.com',
-      }
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
 
-module.exports = withPWA(nextConfig);
+export default withPWA(nextConfig);
