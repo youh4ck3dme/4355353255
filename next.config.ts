114e9c01d8286ddf6fd3aefcb67b70e8647c6d65
Erik Babcan <space@rubberduck.space>
2025-10-23 09:26:39 +0000

    skus to reste raz - diagnosticky test

--- next.config.ts
+++ next.config.ts
@@ -16,14 +16,20 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'viandmo.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
