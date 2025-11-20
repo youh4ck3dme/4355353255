558bbb0e957323a69680ddadb6a923327f546f0c
Erik Babcan <space@rubberduck.space>
2025-11-20 22:26:40 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -1,5 +1,5 @@
 // This file exports the configuration for Firebase.
-// It is used by PublicLayout.tsx to initialize the Firebase app.
+// It is used by the Firebase provider to initialize the app.
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBTILhRT-8_vRTy_DuB8bnLBeX1oygopQw",
@@ -7,6 +7,6 @@ export const firebaseConfig = {
   projectId: "studio-6196097112-43509",
   storageBucket: "studio-6196097112-43509.firebasestorage.app",
   messagingSenderId: "477237695411",
-  appId: "1:477237695411:web:7af4edbc899dbf86d0f1c6",
-  measurementId: "G-SJMZBF8Z60"
+  appId: "1:477237695411:web:4460ae5b555dce29d0f1c6",
+  measurementId: "G-P9QE2C6G6C"
 };
